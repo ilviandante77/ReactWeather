bcbf8bd36b0c0049fbb0a0155c9d75152e252e63
Alberto M <a.marchioni@idshooter.com>
2016-07-24 16:09:29 +0200

    preparato repo for herku

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ var express= require('express');
 
 // creo la mia applicazione
 var app=express();
+const PORT = process.env.PORT || 3300;
+app.use(function(req, res, next){
+  if(req.headers['x-forwarded-proto'] === 'http'){
+    next();
+  } else{
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 app.use(express.static('app'));
 
+
 app.listen(3300, function(){
-  console.log("Express server is run up port 3300");
+  console.log("Express server is run up port" + PORT);
 });
