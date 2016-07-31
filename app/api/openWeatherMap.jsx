53a6cfc9a34577c15f75c7356babf9c73649353a
Alberto M <a.marchioni@idshooter.com>
2016-07-31 18:19:54 +0200

    sostituito api

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,23 +1,20 @@
 var axios = require('axios');
 
-const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=a4c3d4ece7a07bc0bf0aefc19040678b&units=imperial';
-
-
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=c4e735ea8bd7e7b6dc8368c752517b2d&units=imperial';
 
 module.exports = {
   getTemp: function (location) {
     var encodedLocation = encodeURIComponent(location);
-    var requestUrl = '${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}';
-    console.log(requestUrl);
-      return axios.get(requestUrl).then(function  (res){
-        if(res.data.cod && res.data.message){
-          throw new Error(res.data.message);
-          requestUrl;
-        } else {
-          return res.data.main.temp;
-        }
-      }, function (res){
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+
+    return axios.get(requestUrl).then(function (res) {
+      if (res.data.cod && res.data.message) {
         throw new Error(res.data.message);
-      });
+      } else {
+        return res.data.main.temp;
+      }
+    }, function (res) {
+      throw new Error(res.data.message);
+    });
   }
 }
--- app/bundle.js
+++ app/bundle.js
@@ -25289,17 +25289,16 @@
 
 	var axios = __webpack_require__(222);
 
-	var OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=a4c3d4ece7a07bc0bf0aefc19040678b&units=imperial';
+	var OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=c4e735ea8bd7e7b6dc8368c752517b2d&units=imperial';
 
 	module.exports = {
 	  getTemp: function getTemp(location) {
 	    var encodedLocation = encodeURIComponent(location);
-	    var requestUrl = '${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}';
-	    console.log(requestUrl);
+	    var requestUrl = OPEN_WEATHER_MAP_URL + '&q=' + encodedLocation;
+
 	    return axios.get(requestUrl).then(function (res) {
 	      if (res.data.cod && res.data.message) {
 	        throw new Error(res.data.message);
-	        requestUrl;
 	      } else {
 	        return res.data.main.temp;
 	      }
@@ -26521,7 +26520,12 @@
 	      React.createElement(
 	        'p',
 	        null,
-	        'I\'m examples component'
+	        'I\'m examples component !'
+	      ),
+	      React.createElement(
+	        'p',
+	        null,
+	        'Welcome to Examples page'
 	      )
 	    );
 	  }
